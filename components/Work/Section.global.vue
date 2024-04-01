6b1b54b9b6e4dc08ff3bc82c6f5cb2c7d46dd524
tiantsoash98 <tiantsoa.sh@gmail.com>
2024-04-01 11:49:44 +0300

    Update Section.global.vue

--- components/Work/Section.global.vue
+++ components/Work/Section.global.vue
@@ -1,12 +1,12 @@
 <template>
-    <section class="work-section mb-10 mb-md-30">
+    <section class="work-section mb-10 mb-md-30" ref="targetSection">
         <div class="container">
             <div class="row">
                 <div class="work-section__title-wrapper col-full col-md-3 col-lg-5 col-start-md-2">
                     <h6 class="paragraph-text" v-if="title">{{ title }}</h6>
                 </div>
                 <div class="work-section__text-wrapper col-full col-md-7 col-lg-5">
-                    <p class="work-section__text title-h5"><ContentSlot unwrap="p"/></p>
+                    <p class="work-section__text title-h5 section-reveal__block"><ContentSlot unwrap="p"/></p>
                 </div>
             </div>
         </div>
@@ -16,6 +16,23 @@
 <script setup>
 const props = defineProps({
     title: String,
+})
+const targetSection = ref(null)
+const { gsap } = useGsap()
+const { textBlockReveal } = useSectionScrollRevealAnimate()
+
+
+onMounted(() => {
+    gsap.timeline({
+        scrollTrigger: {
+            trigger: targetSection.value,
+            //trigger element - viewport
+            start: "top 80%",
+            end: "top center",
+            toogleActions: "play restart restart none",
+        }
+    })
+    .add(textBlockReveal(targetSection))
 })
 </script>
 
