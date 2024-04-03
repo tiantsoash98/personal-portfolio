782fb7cbfabf9a119954328cdb744d1704773607
tiantsoash98 <tiantsoa.sh@gmail.com>
2024-04-03 20:38:45 +0300

    Update Banner.global.vue

--- components/Work/Banner.global.vue
+++ components/Work/Banner.global.vue
@@ -1,9 +1,9 @@
 <template>
-    <section class="work-banner mb-16 mb-md-30">
+    <section class="work-banner mb-16 mb-md-30" ref="targetSection">
         <div class="work-banner__wrapper img-wrapper">
             <nuxt-img
                 :src="img"
-                class="work-hero__img img img-load" 
+                class="work-banner__img img img-load" 
                 sizes="sm:100vw md:100vw lg:100vw 100vw"
                 role="presentation"
                 loading="lazy"
@@ -15,6 +15,28 @@
 <script setup>
 const props = defineProps({
     img: String,
+})
+const { gsap } = useGsap()
+const targetSection = ref(null)
+
+onMounted(() => {
+    gsap.timeline({
+        scrollTrigger: {
+            trigger: targetSection.value,
+            //trigger element - viewport
+            start: "top bottom",
+            end: "bottom top",
+            scrub: true,
+        }
+    })
+    .set(targetSection.value.querySelector('.work-banner__img'), { 
+        scale: 1.2
+    })
+    .fromTo(targetSection.value.querySelector('.work-banner__img'), { 
+        yPercent: -8
+    }, {
+        yPercent: 8
+    })
 })
 </script>
 
